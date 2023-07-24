0577793f5bbef2c6cc3f25f68fbed10fa5544ed7
Pernille-B <139982159+Pernille-B@users.noreply.github.com>
2023-07-24 11:55:38 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,10 @@
-function skillsMember()git add member.js
+function skillsMember()function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django'];
+    var skillsList = document.getElementById('skills');
+
+    for (var i = 0; i < skills.length; i++) {
+        var skill = document.createElement('li');
+        skill.textContent = skills[i];
+        skillsList.appendChild(skill);
+    }
+}
